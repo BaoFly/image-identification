6eed0c48eadb853790c7ae59bcc40342f2363244
bf <baoFly913@gmail.com>
2023-10-22 15:34:31 +0800

    <fix><修复Vo重复属性歧义问题>

--- Image-Identification/src/main/java/com/bf/image/vo/DetailInformationVo.java
+++ Image-Identification/src/main/java/com/bf/image/vo/DetailInformationVo.java
@@ -1,6 +1,11 @@
 package com.bf.image.vo;
 
+import com.baomidou.mybatisplus.annotation.TableField;
+import com.baomidou.mybatisplus.annotation.TableId;
 import com.bf.image.pojo.DetailInformation;
+import com.bf.image.pojo.DeviceInformation;
+import com.bf.image.pojo.ImageInformation;
+import com.bf.image.pojo.UserInformation;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
@@ -11,12 +16,88 @@ import java.util.Date;
 @Data
 @NoArgsConstructor
 @AllArgsConstructor
-public class DetailInformationVo extends DetailInformation {
+public class DetailInformationVo {
 
+
+    private Long detailId;
+
+    /**
+     * 供电局/区局
+     */
+    private String powerSupplyStation;
+
+    /**
+     * 变电站名称
+     */
+    private String substationName;
+
+    /**
+     * 巡检班组
+     */
+    private String inspectionTeam;
+
+    /**
+     * 环境温度
+     */
+    private Double ambientTemp;
+
+    /**
+     * 相对湿度
+     */
+    private Double relativeHumidity;
+
+    /**
+     * 中心湿度
+     */
+    private Double centralHumidity;
+
+    /**
+     * 最大温度
+     */
+    private Double maxTemp;
+
+    /**
+     * 最小温度
+     */
+    private Double minTemp;
+
+    /**
+     * 平均温度
+     */
+    private Double avgTemp;
+
+    /**
+     * 馈线
+     */
+    private Double feeder;
+
+    /**
+     * 反射温度
+     */
+    private Double emissionTemp;
+
+    /**
+     * 创建时间
+     */
+    private Date createTime;
+
+    /**
+     * 修改时间
+     */
+    private Date updateTime;
+
+    /**
+     * 前端传过来的字段，支持MultipartFile类型 和 base64的图片
+     */
+    private Object imageObj;
+
+    private static final long serialVersionUID = 1L;
+
+    /**
+     * 设备表ID
+     */
     private Long deviceId;
-    private Long imageId;
-    private Long userId;
-    private Long workLeaderId;
+
     /**
      * 0：开关柜 1：变压器 2：电缆 3：其他
      */
@@ -26,6 +107,20 @@ public class DetailInformationVo extends DetailInformation {
      * 设备名称
      */
     private String deviceName;
+
+    private Long userId;
+
+    /**
+     * 用户名
+     */
+    private String username;
+
+    private Long workLeaderId;
+
+    /**
+     * 图片ID
+     */
+    private Long imageId;
 
     /**
      * 图片大小，单位为字节数
@@ -48,28 +143,8 @@ public class DetailInformationVo extends DetailInformation {
     private String storageName;
 
     /**
-     * 创建时间
-     */
-    private Date createTime;
-
-    /**
-     * 更新时间
-     */
-    private Date updateTime;
-
-    /**
      * 创建人名称
      */
     private String creatorName;
-
-    /**
-     * 用户名
-     */
-    private String username;
-
-    /**
-     * 用户密码
-     */
-    private String password;
 
 }
